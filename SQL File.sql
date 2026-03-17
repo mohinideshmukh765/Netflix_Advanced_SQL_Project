275159c129d9b7caa22fa1a0f8f91a56f4838df0
mohinideshmukh765 <138604275+mohinideshmukh765@users.noreply.github.com>
2026-03-17 16:14:54 +0530

    Add files via upload

---
+++ SQL File.sql
@@ -0,0 +1,15 @@
+--Netflix  Project 
+
+create table netflix ( 
+	show_id	VARCHAR(6),
+	type		
+	title	
+	director	
+	cast	
+	country	
+	date_added	
+	release_year	
+	rating	duration	
+	listed_in	
+	description
+)
